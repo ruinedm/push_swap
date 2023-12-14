d6579b14b93346bbe7e72727605c1d659a39a068
unknown <Dynamic>
2023-12-14 06:49:19 +0100

    stuff

--- push_swap.c
+++ push_swap.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "push_swap.h"
+
 
 int check_duplicates(int *stack,int stack_size)
 {
---
+++ push_swap.h
@@ -0,0 +1,8 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#endif
