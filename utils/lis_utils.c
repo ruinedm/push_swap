9a6e04ae1ac8e06c7af34350eb525122e4bc1610
Mohamed Amine <133964099+ruinedm@users.noreply.github.com>
2024-01-11 05:29:44 +0100

    Update lis_utils.c

--- utils/lis_utils.c
+++ utils/lis_utils.c
@@ -1,71 +1,75 @@
-// #include "../push_swap.c"
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_int_arr(int *arr, int arr_size)
-{
-    int i;
-
-    i = 0;
-    while(i < arr_size)
-    {
-        printf("OUTPUT %i: %i\n",i ,arr[i]);
+void print_int_arr(int *arr, int arr_size) {
+    int i = 0;
+    while (i < arr_size) {
+        printf("OUTPUT %i: %i\n", i, arr[i]);
         i++;
     }
 }
-int get_latest_index(int *arr,int arr_size, int num)
-{
-    int i;
-    int latest_index;
 
-    i = 0;
-    while(i < arr_size)
-    {
-        if(arr[i] == num)
+int get_latest_index(int *arr, int arr_size, int num) {
+    int i = 0;
+    int latest_index = -1;
+
+    while (i < arr_size) {
+        if (arr[i] == num)
             latest_index = i;
         i++;
     }
     return latest_index;
 }
-int max(int *arr, int arr_size)
-{
-    int i;
-    int max;
 
-    i = 0;
-    max = 0;
-    while(i < arr_size)
-    {
-        if(max < arr[i])
-            max = arr[i];
+int max(int *arr, int arr_size) {
+    int i = 0;
+    int max_value = arr[0];
+
+    while (i < arr_size) {
+        if (max_value < arr[i]) 
+            max_value = arr[i];
         i++;
     }
-    return (max); 
+    return max_value;
 }
-int get_lis_length(void)
-{
-    int arr[] = {0, 4, 12, 2, 10, 6, 9, 13, 3, 11, 7, 15};
-    int length[12];
-    int subsequence[12];
+
+int get_lis_length(int *arr, int arr_size) {
     int i;
-    int temp;
     int j;
+    int temp;
     int index;
-    int value;
     int len;
+    int *length;
+    int *subsequence;
+    int *list;
+
+    length = (int *)malloc(arr_size * sizeof(int));
+    subsequence = (int *)malloc(arr_size * sizeof(int));
+    if (!length || !subsequence) 
+    {
+        free(length);
+        free(subsequence);
+        return -1;
+    }
 
     i = 1;
     j = 0;
-    while(j < 12) {length[j] = 1; subsequence[j] = -1; j++;}  // Each element is an increasing subsequence of length 1 //
-    j = 0;
-    while(i < 12) // size - 1  iterations??? //
+    while (j < arr_size) 
+    {
+        length[j] = 1;
+        subsequence[j] = -1;
+        j++;
+    }
+
+    while (i < arr_size) 
     {
         j = 0;
-        while(j < i) // i = 3 // j = 2
+        while (j < i) 
         {
-            if(arr[j] < arr[i])
+            if (arr[j] < arr[i]) 
             {
-                temp = length[j] + 1; 
-                if(temp >= length[i])
+                temp = length[j] + 1;
+                if (temp >= length[i]) 
                 {
                     length[i] = temp;
                     subsequence[i] = j;
@@ -75,22 +79,36 @@ int get_lis_length(void)
         }
         i++;
     }
-    len = max(length, 12);
-    int list[len];
-    int x = len - 1;
-    index = get_latest_index(length, 12, len);
-    while(index != -1)
+
+    len = max(length, arr_size);
+    list = (int *)malloc(len * sizeof(int));
+    if (!list) 
     {
-        list[x] = arr[index];
-        x--;
+        free(length);
+        free(subsequence);
+        return -1;
+    }
+
+    index = get_latest_index(length, arr_size, len);
+    i = len - 1;
+    while (index != -1) 
+    {
+        list[i--] = arr[index];
         index = subsequence[index];
     }
 
     print_int_arr(list, len);
+
+    free(length);
+    free(subsequence);
+    free(list);
     return len;
 }
 
-int main()
+int main() 
 {
-    printf("LIS LEN: %i\n", get_lis_length());
+    int arr[] = {0, 4, 12, 2, 10, 6, 9, 13, 3, 11, 7, 15};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    printf("LIS LEN: %i\n", get_lis_length(arr, arr_size));
+    return 0;
 }
